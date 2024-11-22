f2a60629d4abe5e8fd132e5619eb57aa03b6b085
ofekkatzav <ofek53katzav1@gmail.com>
2024-11-22 23:02:27 +0200

    saved

--- Algebra.java
+++ Algebra.java
@@ -7,7 +7,7 @@ public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
 	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
+	    System.out.println(minus(8,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
@@ -25,21 +25,31 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for(int i=0;i<x2;i++){
+			x1++;
+		}
+
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for(int i=0;i<x2;i++){
+			x1--;
+		}
+
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		for(int i=0;i<x1;i++){
+			result = plus(result,x2);
 	}
+	return result;
+	}
+	
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
